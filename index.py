2e2ee7172b5902570dd0c20595c1c17f21232159
B293-Rajesh <rajeshbora010@gmail.com>
2025-08-22 10:28:50 +0530

    Update index.py

--- index.py
+++ index.py
@@ -2,7 +2,7 @@ from textbook_utils import load_textbook, chunk_text, index_textbook
 
 if __name__ == "__main__":
     # Load your PDF
-    text = load_textbook("x_biologyA_em.pdf")
+    text = load_textbook("x_biology_em.pdf")
     chunks = chunk_text(text)
     model, store = index_textbook(chunks)
 
